d0661f9ed0db4926c7ccbac9f4b609c86e0fda08
Lee Cookson <1561172+leecookson@users.noreply.github.com>
2025-06-22 11:10:18 -0400

    add subscription_id to azure provider

--- azure/provider.tf
+++ azure/provider.tf
@@ -8,5 +8,8 @@ terraform {
 }
 
 provider "azurerm" {
+  # When doing a plan on CirleCI, it wants to change the "owner" to the 
+  # service principal, and error indicates it needs subscription_id on the provider
+  subscription_id = var.azure_subscription_id
   features {}
 }
